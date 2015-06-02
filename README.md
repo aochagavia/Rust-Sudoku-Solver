96d17a6a61a4443a6682dcc018cf45a08e0cfca2
Adolfo Ochagavía <aochagavia92@gmail.com>
2015-06-02 16:01:39 +0200

    Extract sudoku mod into external crate

--- README.md
+++ README.md
@@ -1,27 +1,16 @@
 Rust-Sudoku-Solver
 ==================
 
-A basic application to solve sudokus using the Rust programming language 0.11-pre.
+A basic application to solve sudokus using the Rust programming language.
 
-
-##Why?
+## Why?
 
 Because I needed an excuse to program something in Rust, and I was curious about if I could make a non-bruteforcing algorithm to solve a sudoku.
 
+## How does it work?
 
-##How does it work?
+Look at the documentation for the `sudoku` crate
 
-###Algorithm
+## Hey! Something is wrong in your code!
 
-It tries first applying a fast_solve algorithm, which for most sudokus is enough.
-If this does not succeed, then the program proceeds with brute forcing.
-
-Detailed description is provided as comments at the beginning of each source file inside "src/sudoku".
-
-###Input format
-
-The input format is very intuitive and you will surely understand it when you take a look at the files inside the directory "samples"
-
-##Hey! Something is wrong in your code!
-
-Then you can open a pull request. I promise I will look at it.
+Then you can open a pull request!
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
-/*
-
-Simple sudoku solver in Rust
-
-For details on the implementation of the Sudoku look at sudoku/mod.rs
-
-*/
+extern crate sudoku;
 
 use std::fs::File;
 use std::io::BufReader;
 use std::env;
 
-use sudoku::{Sudoku, BruteForce};
-
-mod sudoku;
+use sudoku::Sudoku;
 
 fn main() {
     println!("---------------------");
@@ -27,27 +19,28 @@ fn main() {
     }
 
     let file = File::open(&args[1]).unwrap();
-    let mut sudoku = Sudoku::new(BufReader::new(file));
+    let mut sudoku = Sudoku::from_reader(BufReader::new(file)).unwrap();
 
     // Apply brute force directly if "-b" is specified as argument
     if args.len() > 2 && &args[2] == "-b" {
         println!("Brute forcing...");
-        sudoku.brute_force();
+        sudoku.brute_force_solve();
     } else {
         sudoku.fast_solve();
     }
 
     // Fast solve doesn't always have success
-    if !sudoku.is_completed() {
+    if !sudoku.is_solved() {
         println!("No solution found with fast method, attempting brute force...");
-        if !sudoku.brute_force() {
+        sudoku.brute_force_solve();
+        if !sudoku.is_solved() {
             println!("Brute force failed, make sure that the sudoku is valid.");
             println!("Current solution (for debugging purposes): ");
         }
     }
 
     // Maybe it is now completed
-    if sudoku.is_completed() {
+    if sudoku.is_solved() {
         println!("A solution for \"{}\" has been found!", args[1]);
     }
 
